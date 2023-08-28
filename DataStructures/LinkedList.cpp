d20eec8f82128f70e45c53191fa83683c34d7eb0
Mayank Pallai <mayankpallai@gmail.com>
2023-08-29 01:16:21 +0800

    added linkedList

--- DataStructures/LinkedList.cpp
+++ DataStructures/LinkedList.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    
-    public:
+class Node {
+public:
     int data;
-    Node *next;
+    Node* next;
 
-    Node(int data){
-        this -> data = data;
-        next = NULL; 
+    Node(int data) {
+        this->data = data;
+        next = NULL;
     }
 };
 
-int main(){
+void print(Node* head) {
+    while (head != NULL) {
+        cout << head->data << ' ';
+        head = head->next;
+    }
+}
+
+int main() {
+
     //Statically
+    Node n1(1);   // Head node
+    Node* head = &n1;
 
-    Node n1(1);   //head node
-    Node *head = &n1;
-
-    Node n2(2);    
+    Node n2(2);
     Node n3(3);
     Node n4(4);
     Node n5(5);
@@ -29,17 +35,14 @@ int main(){
     n3.next = &n4;
     n4.next = &n5;
 
-   
-    // n1.next = &n2;
+    print(head);
 
-    // cout << head -> data << ' ' << n2.data;
-    
-    // //Dynamically
+    //Dynamically
     // Node *n3 = new Node(10);
     // Node *head = n3;
 
     // Node *n4 = new Node(20);
     // n3 -> next = n4;
-    
+
     return 0;
 }
