cfda7f0a462c79be1a112c24b6d9bb4aff83e312
Mayank Pallai <mayankpallai@gmail.com>
2023-08-30 14:22:49 +0800

    updated linkedList

--- DataStructures/LinkedList.cpp
+++ DataStructures/LinkedList.cpp
@@ -12,6 +12,19 @@ public:
     }
 };
 
+Node * takeInput(){
+    int data;
+    cin >> data;
+    Node *head = NULL:
+    while(data != -1){
+        Node *newNode = new Node(data);
+        if(head == NULL){
+            head = newNode;
+        }
+        cin >> data;
+    }
+}
+
 void print(Node* head) {
     Node *temp = head;
     while (temp != NULL) {
