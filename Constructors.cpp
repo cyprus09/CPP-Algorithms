99c1c126b41f1372a55aefb57df64605fbfcc650
Mayank Pallai <mayankpallai@gmail.com>
2023-09-01 20:48:50 +0800

    constructors implementation added

--- Constructors.cpp
+++ Constructors.cpp
@@ -1,67 +1,214 @@
+#include <vector>
+#include <climits>
 #include <iostream>
 using namespace std;
 
-class DynamicArray {
-    int *data;
-    int nextIndex;
-    int capacity; //total size
+class Polynomial {
+
+    public:
+
+    int *degCoeff;      // Name of your array (Don't change this)
+    int capacity;
     
-public:
-    DynamicArray(){
-        data = new int[5];
-        nextIndex = 0;
-        capacity = 5;
+    Polynomial(){
+        this->degCoeff=new int[6];
+        this->capacity=5;
+    }
+    //Making paramaterized constructor for defining with capacity
+    Polynomial (int capacity){
+        this->degCoeff=new int[capacity+1];
+        this->capacity=capacity;
     }
     
-    void add(int element){
-        if(nextIndex == capacity){
-            int *newData = new int[2 * capacity];
-            for(int i = 0; i < capacity; i++){
-                newData[i] = data[i];
+    Polynomial (Polynomial const &p){
+        int *newdeg=new int[p.capacity+1];
+            
+        for(int i=0;i<=p.capacity;i++)
+            newdeg[i]=p.degCoeff[i];
+            
+        this->degCoeff=newdeg;
+        
+        this->capacity=p.capacity;
+    }
+    
+    void setCoefficient(int deg,int coef){
+        if(deg>capacity){
+            int newcapacity=deg;
+            int *newdeg=new int[newcapacity+1];
+            //Copy the contents from original to new
+            for(int i=0;i<=capacity;i++)
+                newdeg[i]=degCoeff[i];
+            
+            this->degCoeff=newdeg;
+            this->capacity=newcapacity;
+            //Set the new coeff;
+            degCoeff[deg]=coef;
+        }
+        else{
+            degCoeff[deg]=coef;
+        }
+    }
+    
+    Polynomial operator+(Polynomial const &P2){
+        
+        int newcap=max(this->capacity,P2.capacity);
+        
+        Polynomial P3(newcap);
+        
+        for(int i=0;i<=newcap;i++){
+            if(i<=capacity && i<=P2.capacity)
+                P3.degCoeff[i]=this->degCoeff[i]+P2.degCoeff[i];
+            else if(i<=capacity)
+                P3.degCoeff[i]=this->degCoeff[i];
+            else 
+                P3.degCoeff[i]=P2.degCoeff[i];
+        }
+        
+        return P3;
+    }
+    
+    Polynomial operator-(Polynomial const &P2){
+        
+        int newcap=max(this->capacity,P2.capacity);
+        Polynomial P3(newcap);
+        
+        for(int i=0;i<=newcap;i++){
+            if(i<=capacity && i<=P2.capacity)
+                P3.degCoeff[i]=this->degCoeff[i]-P2.degCoeff[i];
+            else if(i<=capacity)
+                P3.degCoeff[i]=this->degCoeff[i];
+            else 
+                P3.degCoeff[i]=-P2.degCoeff[i];
+        }
+        
+        return P3;
+    }
+    
+    Polynomial operator*(Polynomial const &P2){
+        
+        int newcap=this->capacity+P2.capacity;
+        Polynomial P3(newcap);
+        
+        for(int i=0;i<=this->capacity;i++){
+            
+            for(int j=0;j<=P2.capacity;j++){
+                P3.degCoeff[i+j]+=this->degCoeff[i]*P2.degCoeff[j];
             }
-            delete [] data;
-            data = newData;
-            capacity *= 2;
         }
-        data[nextIndex] = element;
-        nextIndex++;
+        return P3;
     }
     
-    int get(int i){
-        if(i < nextIndex){
-            return data[i];
-        }else{
-            return -1;
-        }
+    void operator=(Polynomial const &p){
+        int *newdeg=new int[p.capacity+1];
+        //Copy the contents
+        for(int i=0;i<p.capacity;i++)
+            newdeg[i]=p.degCoeff[i];
+            
+        
+        this->degCoeff=newdeg;
+        
+        this->capacity=p.capacity;
     }
     
-    void add(int i , int element){
-        if(i < nextIndex){
-            data[i] = element;
-        }else if(i == nextIndex){
-            add(element);
-        }else{
-            return;
+    void print(){
+        
+        for(int i=0;i<=this->capacity;i++){
+            if(degCoeff[i]!=0)
+                cout<<degCoeff[i]<<"x"<<i<<" ";
         }
-    }
-    
-    void print() {
-        for(int i = 0; i < nextIndex; i++){
-            cout << data[i] << ' ';
-        }
-        cout << endl;
+        cout<<endl;
     }
 };
 
-int main() {
-    DynamicArray d1;
+
+int main()
+{
+    int count1,count2,choice;
+    cin >> count1;
     
-    d1.add(10);
-    d1.add(20);
-    d1.add(30);
-    d1.add(40);
-    d1.add(50);
-    d1.add(60);
+    int *degree1 = new int[count1];
+    int *coeff1 = new int[count1];
     
-    d1.print();
+    for(int i=0;i < count1; i++) {
+        cin >> degree1[i];
+    }
+    
+    for(int i=0;i < count1; i++) {
+        cin >> coeff1[i];
+    }
+    
+    Polynomial first;
+    for(int i = 0; i < count1; i++){
+        first.setCoefficient(degree1[i],coeff1[i]);
+    }
+    
+    cin >> count2;
+    
+    int *degree2 = new int[count2];
+    int *coeff2 = new int[count2];
+    
+    for(int i=0;i < count2; i++) {
+        cin >> degree2[i];
+    }
+    
+    for(int i=0;i < count2; i++) {
+        cin >> coeff2[i];
+    }
+    
+    Polynomial second;
+    for(int i = 0; i < count2; i++){
+        second.setCoefficient(degree2[i],coeff2[i]);
+    }
+    
+    cin >> choice;
+    
+    switch(choice){
+            // Add
+        case 1:
+        {
+            Polynomial result1 = first + second;
+            result1.print();
+            break;
+        }
+            // Subtract
+        case 2 :
+        {
+            Polynomial result2 = first - second;
+            result2.print();
+            break;
+        }
+            // Multiply
+        case 3 :
+        {
+            Polynomial result3 = first * second;
+            result3.print();
+            break;
+        }
+        case 4 : // Copy constructor
+        {
+            Polynomial third(first);
+            if(third.degCoeff == first.degCoeff) {
+                cout << "false" << endl;
+            }
+            else {
+                cout << "true" << endl;
+            }
+            break;
+        }
+            
+        case 5 : // Copy assignment operator
+        {
+            Polynomial fourth(first);
+            if(fourth.degCoeff == first.degCoeff) {
+                cout << "false" << endl;
+            }
+            else {
+                cout << "true" << endl;
+            }
+            break;
+        }
+            
+    }
+    
+    return 0;
 }
--- DataStructures/LinkedList.cpp
+++ DataStructures/LinkedList.cpp
@@ -15,17 +15,18 @@ public:
 Node * takeInput(){
     int data;
     cin >> data;
+
     Node *head = NULL;
+    Node *tail = NULL;
+    
     while(data != -1){
         Node *newNode = new Node(data);
         if(head == NULL){
             head = newNode;
+            tail = newNode;
         }else{
-            Node *temp = head;
-            while(temp -> next != NULL){
-                temp = temp -> next;
-            }
-            temp -> next = newNode;
+            tail -> next = newNode;
+            tail = tail -> next;
         }
         cin >> data;
     }
