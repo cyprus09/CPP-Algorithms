2a12aab303a87c1d71d053668fd8ad8542826095
Mayank Pallai <mayankpallai@gmail.com>
2023-09-02 04:37:47 +0800

    added nodeInsertion

--- DataStructures/LinkedList.cpp
+++ DataStructures/LinkedList.cpp
@@ -34,17 +34,25 @@ Node * takeInput(){
     return head;
 }
 
-void insertNode(Node *head, int i, int data){
+Node* insertNode(Node *head, int i, int data){
     Node *newNode = new Node(data);
     int count = 0;
     Node *temp = head;
-    while(count < i - 1){
+    if(i == 0){
+        newNode -> next = head;
+        head = newNode;
+        return head;
+    }
+    while(temp != NULL && count < i - 1){
         temp = temp -> next;
         count++;
     }
-    Node *a = temp -> next;
-    temp -> next = newNode;
-    newNode -> next = a;
+    if(temp != NULL){
+        Node *a = temp -> next;
+        temp -> next = newNode;
+        newNode -> next = a;
+    }
+    return head;
 }
 
 void print(Node* head) {
@@ -61,28 +69,19 @@ int main() {
     print(head);
     int i, data;
     cin >> i >> data;
-    insertNode(head, i , data);
+    head = insertNode(head, i , data);
     print(head);
 
 
     // //Statically
     // Node n1(1);   // Head node
     // Node* head = &n1;
-
     // Node n2(2);
-    // Node n3(3);
-    // Node n4(4);
-    // Node n5(5);
-
     // n1.next = &n2;
-    // n2.next = &n3;
-    // n3.next = &n4;
-    // n4.next = &n5;
 
     //Dynamically
     // Node *n3 = new Node(10);
     // Node *head = n3;
-
     // Node *n4 = new Node(20);
     // n3 -> next = n4;
 
