43c81128db54759ee02436064fdc296b7d3addd3
Mayank Pallai <mayankpallai@gmail.com>
2023-09-08 22:51:24 +0800

    linkedList slow-fast

--- DataStructures/LinkedList.cpp
+++ DataStructures/LinkedList.cpp
@@ -209,6 +209,23 @@ void print(Node* head){
 
 }
 
+Node *midPoint(Node *head)
+{
+    if(head == NULL){
+       return NULL;
+   }
+
+   Node *slow = head;
+   Node *fast = head -;
+
+   while(fast != NULL && fast -> next != NULL){
+	   slow = slow -> next;
+	   fast = fast -> next -> next;
+   }
+   
+   return slow;
+}
+
 void printReverse(Node *head)
 {
 	Node *curr = head;
@@ -264,6 +281,25 @@ bool isPalindrome(Node *head)
         right = right->next;
     }
 	return true;
+}
+
+bool detectCycle(Node *head)
+{
+    if(head == NULL || head -> next == NULL){
+        return false;
+    }
+    Node *slow = head;
+	Node *fast = head -> next;
+    while(fast != NULL && fast -> next != NULL){
+       slow = slow -> next;
+       fast = fast -> next -> next;
+
+       if(slow == fast){
+        return true;
+    }
+    }
+    
+    return false;
 }
 
 int main() {
