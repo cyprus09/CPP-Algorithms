dc9c5cd12c8a7c6350d2f0ba6cb65eafc0da3dc5
mayankpallai <mayankpallai@gmail.com>
2023-10-27 20:50:57 +0800

    implemented priority queues class

---
+++ PriorityQueues/PriorityQueue.h
@@ -0,0 +1,88 @@
+#include <vector>
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+class PriorityQueue {
+    vector<int> pq;
+
+   public:
+    bool isEmpty() { 
+        return pq.size() == 0; 
+    }
+
+    int getSize() { 
+        return pq.size(); 
+    }
+
+    int getMin() {
+        if (isEmpty()) {
+            return 0;
+        }
+
+        return pq[0];
+    }
+
+    void insert(int element) {
+        pq.push_back(element);
+
+        int childIndex = pq.size() - 1;
+
+        while (childIndex > 0) {
+            int parentIndex = (childIndex - 1) / 2;
+
+            if (pq[childIndex] < pq[parentIndex]) {
+                int temp = pq[childIndex];
+                pq[childIndex] = pq[parentIndex];
+                pq[parentIndex] = temp;
+            } else {
+                break;
+            }
+
+            childIndex = parentIndex;
+        }
+    }
+
+    int removeMin() {
+        if(isEmpty()){
+            return -1;
+        }
+
+        int minVal = getMin();
+
+        int lastIndex = pq.size() - 1;
+        pq[0] = pq[lastIndex];
+        pq.pop_back();
+
+        int parentIndex=0;
+        int leftChildIndex= 1;
+        int rightChildIndex= 2;
+
+        while(leftChildIndex < pq.size()){
+            int minIndex=parentIndex;
+            
+            if(pq[minIndex] > pq[leftChildIndex]){
+                minIndex = leftChildIndex;
+            }
+            
+            if(rightChildIndex < pq.size() && pq[minIndex] > pq[rightChildIndex]){
+                	minIndex = rightChildIndex;
+            }
+            
+            if(minIndex == parentIndex){
+                break;
+            }
+            
+            int temp=pq[parentIndex];
+            pq[parentIndex] = pq[minIndex];
+            pq[minIndex] = temp;
+            
+            parentIndex=minIndex;
+
+            leftChildIndex=2*parentIndex + 1;
+            rightChildIndex=2*parentIndex + 2;
+        }
+        return minVal;
+    }
+};
