f473167c1f93516136ff81938217269294ef09d0
mayankpallai <mayankpallai@gmail.com>
2023-12-28 23:21:56 +0530

    added new methods

--- Recursion/Subsequence.cpp
+++ Recursion/Subsequence.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
 // bool checksequenece(char large[] , char*small) {
@@ -36,6 +36,23 @@ bool checksequenece(char large[] , char*small) {
     checkSequenceRecursive(large, small, 0, 0);
 }
 
+//counting number of subsequences
+int subSequence(string input, string output[]){
+    if(input.empty()){
+        output[0] = '';
+        return 1;
+    }
+
+    string smallString = input.substr(1);
+    int smallOutputSize = subSequence(smallString, output);
+    
+    for(int i = 0; i < smallOutputSize; i++){
+        output[i + smallOutputSize] = input[0] + output[i];
+    }
+
+    return 2 * smallOutputSize;
+}
+
 int main()
 {
 	char large[10000];
@@ -49,6 +66,15 @@ int main()
 	else
 		cout<<"false";
 
+
+    string input;
+    cin >> input;
+    
+    string* output = new string[1000];
+    
+    int count = subSequence(input, output);
+
+    for(int i = 0 ; i < count; i++){
+        cout << output[i] << endl;
+    }
 }
-
-
